30efc235aaae67b3c1ff13e232b7ef139c7a863b
MohammedAlewi <rofyalewi@gmail.com>
2019-11-20 05:51:03 +0300

    finalizing the subtraction alg...

---
+++ Day_2/add_negative_number.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+void subtraction(char *input1,char *input2,char *out);
+void add(char *input1,char *input2,char *out);
+int main(){
+    char input1[200];
+    char input2[200];
+    char out[200];
+    printf(" >>");
+
+    scanf("%s",input1);
+
+    printf(" >>");
+    scanf("%s",input2);
+    
+    subtraction(input1,input2,out);
+    printf("%s",out);
+    return 0;
+}
+
+void add(char *input1,char *input2,char *out){
+    int len1=strlen(input1);
+    int len2=strlen(input2);
+    
+    int length=0;
+    int carry=0;
+    if (len1>len2){
+        length=len1;
+    }else{
+        length=len2;
+    }
+    int i ;
+    for ( i = 0; i < length; i++){
+        int x=0,y=0,over,over2;
+        if((len1-1-i)>=0)x=(input1[len1-1-i])-'0';
+        if((len2-1-i)>=0)y=(input2[len2-1-i])-'0';
+
+        if(carry<1) out[length-i]=(char)(x+y)%10 +'0';
+        else out[length-i]=(char)(x+y+carry)%10 +'0'; 
+
+        carry=(x+y+carry)/10;              
+    }
+    out[length-i]=carry+'0';
+    out[length+1]='\0'; 
+}
+
+void subtraction(char *input1,char *input2,char *out){
+    char *negative_number,*positive_number;
+    
+    negative_number= input1[0]=='-'?input1:input2;
+    positive_number= input1[0]!='-'?input1:input2;
+
+    int negative_number_index=  strlen(negative_number);
+    int length= (strlen(negative_number)-1>strlen(positive_number))?
+            strlen(negative_number)-1:strlen(positive_number);
+
+    negative_number[0]='0';
+    char buffer[200];
+    char one[10];
+    one[0]='1';one[1]='\0';
+
+    int i;
+    int carry=0;
+    for ( i = 0; i < length; i++){
+        int x=0,y=0,over,over2; //
+        if((negative_number_index-i-1)>=0) x=(negative_number[negative_number_index-1-i])-'0';
+        buffer[length-i]=(char)(9-x) +'0';             
+    }
+    buffer[length-i]='0';
+    buffer[length+1]='\0';
+    add(buffer,one,buffer);
+
+    add(buffer, positive_number,buffer);
+    buffer[strlen(buffer)-length-1]='0';
+
+    strcpy(buffer,out);
+    
+}
