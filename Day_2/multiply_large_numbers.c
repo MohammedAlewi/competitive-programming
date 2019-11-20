6c611b63ce75950fbe40ccea5bf1f164caab3118
MohammedAlewi <rofyalewi@gmail.com>
2019-11-20 10:59:27 +0300

    final correction on the multiplication function

--- Day_2/multiply_large_numbers.c
+++ Day_2/multiply_large_numbers.c
@@ -32,6 +32,7 @@ void multiply(char *input1,char *input2,char *out){
     index[1]='\0';
     
     add(index,inc,index);
+    
     add(input2,input2,out);
     while(compare(index,input1)==0){  
         add(input2,out,out);
@@ -41,21 +42,24 @@ void multiply(char *input1,char *input2,char *out){
 
 int compare(char *input1,char *input2){
     int ans=1;
-    int len1=strlen(input1);
-    int len2=strlen(input2);
+    int len1=strlen(input1)-1;
+    int len2=strlen(input2)-1;
     int length=len1>len2? len1:len2;
-    for (int i = 0; i < length; i++){
-        if (input1[len1-i]!=input2[len2-i] && ((len1-i)>=0 && (len2-i)>=0 )){
-           ans=0; 
+    for (int i = 0; i <=length; i++){
+        //printf("\nout : %c %c",input1[len1-i],input2[len2-i]);
+        //1printf("\nn1 :%s n2 :%s comp: %d \n",input1[len1-i]!=input2[len2-i] && ((input1[len1-i])!='0'  && (input1[len2-i])!='0'  ));
+        if ((input1[len1-i]!=input2[len2-i] && ((len1-i)>=0 && (len2-i)>=0) )){
+           return 0; 
         }
     }
+    printf("------- \n");
+
     return ans;
 }
 
 void add(char *input1,char *input2,char *out){
     int len1=strlen(input1);
     int len2=strlen(input2);
-    
     int length=0;
     int carry=0;
     if (len1>len2){
@@ -71,9 +75,11 @@ void add(char *input1,char *input2,char *out){
 
         if(carry<1) out[length-i]=(char)(x+y)%10 +'0';
         else out[length-i]=(char)(x+y+carry)%10 +'0'; 
-
-        carry=(x+y+carry)/10;              
+        
+        carry=(x+y+carry)/10;      
     }
-    out[length-i]=carry+'0';
-    out[length+1]='\0'; 
+     out[length+1]='\0';
+     out[length-i]=carry+'0';
+
+
 }
