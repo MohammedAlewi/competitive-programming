616970160491c695c2d7f14914f292cdc09a03f7
MohammedAlewi <rofyalewi@gmail.com>
2019-12-14 22:25:39 +0300

    contest problems....

--- Codeforces/contest 3.py
+++ Codeforces/contest 3.py
@@ -1,5 +1,24 @@
-def main():
-    pass
+def main(l):
+    x=[l[0],l[0]+1]
+    y=[l[1],l[1]-1,l[1]+1]
+    z=[l[2],l[2]-1]
+    ans=[]
+    for i in x:
+        for j in y:
+            for k in z:
+                ans.append(abs(i-j)+abs(i-k)+abs(j-k))
+    print(min(ans))
 
 
-main()
+def runner():
+    val=input()
+    x=[]
+    for i in range(int(val)):
+        ans=input()
+        ans=ans.split(" ")
+        ans=[int(i) for i in ans]
+        x.append(ans)
+    for i in range(int(val)):
+        main(x[i])
+
+runner()
--- Codeforces/contest2.py
+++ Codeforces/contest2.py
@@ -1,5 +1,27 @@
-def main():
-    pass
+def main(len,nums):
+    length=int(len)
+    nums=nums.split(" ")
+    index=[]
+    val=1
+    flag=True
+    for i in range(1,length):
+        if nums[i-1]>nums[i] and flag:
+            flag=False
+            continue
+        elif nums[i-1]>nums[i]: 
+            index.append(val)
+            flag=True
+            val=1
+            continue
+        val+=1
+    index.append(val)
+    print(max(index))
 
+def runner():
+    length=input()
+    nums=input()
+    main(length,nums)
 
-main()
+length=""
+nums=""
+main(length,nums)
