33c428cd50a343c095d490523073578b72102bcd
MohammedAlewi <rofyalewi@gmail.com>
2019-12-27 21:16:11 +0300

    additional contest questions..

--- leetcode/Sorting Problems/Daily Tempreture.py
+++ leetcode/Sorting Problems/Daily Tempreture.py
@@ -1,18 +1,13 @@
-
 class Solution:
-    def dailyTemperatures(self, T):
-        result=[]
+    def dailyTemperatures(self, T: List[int]) -> List[int]:
+        result=[0]*len(T)
         lenght=len(T)
-        for i in range(lenght):
-            result.append(self.checkWarmTemp(T[i:]))
+        m=[(T[0],0)]
+        for i in range(1,lenght):
+            if len(m)>0 and T[i]>m[len(m)-1][0]:
+                while(len(m)>0 and T[i]>m[len(m)-1][0]):
+                    result[m[len(m)-1][1]]=i-m[len(m)-1][1]
+                    m.pop()
+            m.append((T[i],i))
         return result
         
-    def checkWarmTemp(self,temp):
-        lenght=len(temp)
-        for i in range(1,lenght):
-            if(temp[0]<temp[i]): return i
-        return 0
-
-s=Solution()
-print(s.dailyTemperatures([34,23,43,45,34,12,49,22,34,21,43,121]))
-#[2, 1, 1, 3, 2, 1, 5, 1, 2, 1, 1, 0]
