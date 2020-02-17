32fc0b86d44092941b724b32f97cd7f46bf167ee
Mohammed Alewi <39517049+MohammedAlewi@users.noreply.github.com>
2020-02-17 15:25:43 +0300

    update tmp file

--- leetcode/contests/176/temp_contest_q.py
+++ leetcode/contests/176/temp_contest_q.py
@@ -3,8 +3,8 @@ class Solution:
         events.sort(key=lambda value:value[1])
         dates=set()
         count=0
-        for i in events:
-            for j in range(i[0],i[1]+1):
+        for event in events:
+            for j in range(event[0],event[1]+1):
                 if  j not in dates:
                     dates.add(j)
                     count+=1
