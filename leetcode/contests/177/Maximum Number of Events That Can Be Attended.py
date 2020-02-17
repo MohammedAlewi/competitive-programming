65a570c18adb6962ff6ce170e447faacc688a8fe
MohammedAlewi <rofyalewi@gmail.com>
2020-02-17 15:19:05 +0300

    contest questions....

--- leetcode/contests/177/Maximum Number of Events That Can Be Attended.py
+++ leetcode/contests/177/Maximum Number of Events That Can Be Attended.py
@@ -1,16 +1,13 @@
 class Solution:
     def maxEvents(self, events: List[List[int]]) -> int:
         events.sort(key=lambda value:value[1])
-        dates=[False]*100005
-
+        dates=set()
+        count=0
         for i in events:
             for j in range(i[0],i[1]+1):
-                if dates[j]==False:
-                    dates[j]=True
+                if  j not in dates:
+                    dates.add(j)
+                    count+=1
                     break
                     
-        counts=0
-        for i in dates:
-            if i:
-                counts+=1
-        return counts
+        return count
